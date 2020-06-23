eb08735c78de54d9c6190e8ad76ac869369fea66
Umair0304 <umairdhilon13@gmail.com>
2020-06-23 20:22:06 +0500

    2nd assignment

---
+++ app.js
@@ -0,0 +1,98 @@
+// Chapter 21 to 25
+
+// Q # 1
+
+//var firstName=prompt("Enter Your First Name");
+//var lastName=prompt("Enter Your Last Name");
+//var fullName=firstName+ " " + lastName;
+//alert("Welcome " + fullName );
+
+
+// Q # 2
+
+//var mobilePhone=prompt("Enter Your Favourite Mobile Name");
+//var length=mobilePhone.length;
+//document.write("My favorite phone is: " + mobilePhone + "<br>");
+//for(i=length; i<=length; i++){
+//    document.write("Length of string: " + i)
+//}
+
+// Q # 3
+
+
+//var string="Pakistani";
+//var index=string.indexOf("n");
+//document.write("String: " + string + "<br>");
+//document.write("Index of 'n': " + index);
+
+// Q # 4
+
+//var string="Hello World";
+//var index=string.lastIndexOf("l");
+//document.write("String: " + string + "<br>");
+//document.write("Last index of 'l': " + index);
+
+// Q # 5
+
+//var string="Pakistani";
+//var index=string.charAt(3);
+//document.write("String: " + string + "<br>");
+//document.write("Charachter at index '3': " + index);
+
+
+// Q # 6
+
+
+//var firstName=prompt("Enter Your First Name");
+//var lastName=prompt("Enter Your Last Name");
+//var fullName=firstName + " ".concat(lastName);
+//alert("Welcome " + fullName );
+
+// Q # 7
+
+//var city ="Hyderabad";
+//var replace=city.replace("Hyderabad","Islamabad");
+//document.write("City: " + city + "<br>");
+//document.write("After replacement: " + replace);
+
+// Q # 8
+
+//var message = "Ali and Sami are best friends. They play cricket and football together";
+//var replace=message.replace(/and/g,"&");
+//alert(replace);
+
+
+// Q # 9
+
+//var string="472";
+//var number=parseInt(string);
+//document.write("Value: " + string + "<br>" + "Type: " + "String" + "<br>");
+//document.write("Value: " + number + "<br>" + "Type: " + "Number" + "<br>");
+
+
+// Q # 10;
+
+//var lowerCase=prompt("Enter any word");
+//var upperCase=lowerCase.toLocaleUpperCase();
+//alert(upperCase);
+
+// Q # 11
+
+
+//var input=prompt("Input");
+//var g=input.charAt(0).toUpperCase();
+//var o=input.slice(1).toLowerCase();
+//var p=g.concat(o);
+//document.write("User input: " + input + "<br>");
+//document.write("Title case: " + p);
+
+// Q # 12
+
+// var a=35.36;
+// var t=a.toString().split(".").join("");
+// document.write("Number: " + a + "<br>");
+// document.write("Result: " + t);
+
+// Q # 13
+
+
